83888075c5236f8ef5555dfb5dca74940bf84e9e
Riccardo Colombo <35687949+Ardoriccardo00@users.noreply.github.com>
2019-12-06 17:28:31 +0100

    First commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D characterController;
+    float horizontalMove = 0;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        //characterController.Move(horizontalMove);
+    }
+}
